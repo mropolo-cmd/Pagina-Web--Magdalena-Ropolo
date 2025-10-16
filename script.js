70b4077f22907b653f073564b1acaa7377fdcef9
Magdalena <mropolo@itba.edu.ar>
2025-10-16 10:06:07 -0300

    hacer que se vea el menu

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ if (soundBtn && video && soundIcon) {
 // =======================MENÚ DESPLEGABLE MÓVIL ===========================
 const menuToggle = document.getElementById('menuToggle');
 const navLinks = document.getElementById('navLinks');
+const overlay = document.getElementById('overlay');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
